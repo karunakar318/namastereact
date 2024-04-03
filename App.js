8a7fd3521284019a9bb78791211f7640c620b7d0
Karunakar <narlakondakarunakar2k18@gmail.com>
2024-04-03 20:14:44 +0530

    EPISODE-1

---
+++ App.js
@@ -0,0 +1,8 @@
+let heading =React.createElement("h1",{id:"heading",xyz:"abc"},"hello world");
+let parent=React.createElement("div",{id:"parent"},[React.createElement("div",{id:"child"},[heading,heading]),React.createElement("div",{id:"child"},[heading,heading])])
+
+let root=ReactDOM.createRoot(document.getElementById("root"));
+console.log(document.getElementById("root"));
+console.log(parent);
+console.log(root.render(parent))
+     
